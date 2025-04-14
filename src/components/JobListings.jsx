fe2651eb5f8d40feeac1fe93f1c9a44d3453cb23
Akash Shrestha <41756657+akashstha@users.noreply.github.com>
2025-04-14 17:12:14 -0500

    [JobPortal]adding few Job Listing components for Job Portal App

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,7 +1,7 @@
 import Jobs from "../jobs.json";
 import { JobListing } from "./JobListing";
 export const JobListings = () => {
-  // const jobs = Jobs
+  const recentJobs = Jobs.slice(0, 3);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -9,7 +9,7 @@ export const JobListings = () => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {Jobs.map((job) => (
+          {recentJobs.map((job) => (
             <>
               <JobListing job={job} key={job.id} />
             </>
