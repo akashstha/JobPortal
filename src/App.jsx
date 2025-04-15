9314968e567755c8121a6cee53620b690db73b94
Akash Shrestha <41756657+akashstha@users.noreply.github.com>
2025-04-14 22:40:07 -0500

    [JobPortal]adding Page Not Found pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import { MainLayouts } from './components/layouts/MainLayouts';
 import { HomePage } from './components/pages/HomePage';
 import { JobsPages } from './components/pages/JobsPage';
+import { NotFoundPage } from './components/pages/NotFoundPage';
 const router = createBrowserRouter(createRoutesFromElements(
 <Route path="/" element={<MainLayouts/>}>
 <Route index element={<HomePage/>} />
 <Route path="/jobs" element={<JobsPages/>} />
+<Route path="*" element={<NotFoundPage/>} />
 </Route>
 )
 );
